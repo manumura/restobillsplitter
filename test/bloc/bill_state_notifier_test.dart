fd96756ce17afa927cd7c0a999f0a8f6445264c8
Emmanuel Mura <emmanuel.mura@siriustech.io>
2022-12-30 12:36:36 +0700

    update dependencies + unit tests

---
+++ test/bloc/bill_state_notifier_test.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:restobillsplitter/bloc/bill_state_notifier.dart';
+import 'package:restobillsplitter/models/dish_model.dart';
+import 'package:restobillsplitter/models/guest_model.dart';
+
+void main() {
+
+  late BillStateNotifier billStateNotifier;
+
+  setUp(() async {
+    billStateNotifier = BillStateNotifier();
+  });
+
+  group('BillStateNotifier', () {
+    test('should add,edit,remove guest successfully', () async {
+      // Add
+      billStateNotifier.addGuest();
+
+      // final BillModel bill = billStateNotifier.state;
+      expect(billStateNotifier.state.guests, isNotEmpty);
+      final GuestModel guest1 = billStateNotifier.state.guests[0];
+      expect(guest1, isNotNull);
+      expect(guest1.name, 'Guest1');
+      expect(guest1.uuid, isNotNull);
+      expect(guest1.color, isNotNull);
+      expect(guest1.dishes, <DishModel>[]);
+
+      // Edit
+      final GuestModel guestToEdit = GuestModel(uuid: guest1.uuid, name: 'Guest1!!!', color: guest1.color, dishes: <DishModel>[],);
+      billStateNotifier.editGuest(guestToEdit);
+
+      expect(billStateNotifier.state.guests, isNotEmpty);
+      final GuestModel guest1Edited = billStateNotifier.state.guests[0];
+      expect(guest1Edited, isNotNull);
+      expect(guest1Edited.name, 'Guest1!!!');
+      expect(guest1Edited.uuid, isNotNull);
+      expect(guest1Edited.color, isNotNull);
+      expect(guest1Edited.dishes, <DishModel>[]);
+
+      // Remove
+      billStateNotifier.removeGuest(guestToEdit);
+
+      expect(billStateNotifier.state.guests, isEmpty);
+    });
+
+    test('should add,edit,remove dish successfully', () async {
+      // Add
+      billStateNotifier.addDish();
+
+      // final BillModel bill = billStateNotifier.state;
+      expect(billStateNotifier.state.dishes, isNotEmpty);
+      final DishModel dish1 = billStateNotifier.state.dishes[0];
+      expect(dish1, isNotNull);
+      expect(dish1.name, 'Dish1');
+      expect(dish1.uuid, isNotNull);
+      expect(dish1.price, 0.0);
+      expect(dish1.guestUuids, isNull);
+
+      // Edit
+      final DishModel dishToEdit = DishModel(uuid: dish1.uuid, name: 'Dish1!!!', price: 1.0, guestUuids: <String>[],);
+      billStateNotifier.editDish(dishToEdit);
+
+      expect(billStateNotifier.state.dishes, isNotEmpty);
+      final DishModel dish1Edited = billStateNotifier.state.dishes[0];
+      expect(dish1Edited, isNotNull);
+      expect(dish1Edited.name, 'Dish1!!!');
+      expect(dish1Edited.uuid, isNotNull);
+      expect(dish1Edited.price, 1.0);
+      expect(dish1Edited.guestUuids, <String>[]);
+
+      // Remove
+      billStateNotifier.removeDish(dishToEdit);
+
+      expect(billStateNotifier.state.dishes, isEmpty);
+    });
+
+    test('should assign guest to dish successfully', () async {
+      // Add dish
+      billStateNotifier.addDish();
+      // Add guests
+      for (int i = 0; i < 2; i++) {
+        billStateNotifier.addGuest();
+      }
+
+      expect(billStateNotifier.state.dishes, isNotEmpty);
+      expect(billStateNotifier.state.dishes.length, 1);
+      expect(billStateNotifier.state.guests, isNotEmpty);
+      expect(billStateNotifier.state.guests.length, 2);
+
+      billStateNotifier.assignGuestsToDish(billStateNotifier.state.dishes[0], billStateNotifier.state.guests);
+
+      final DishModel dish = billStateNotifier.state.dishes[0];
+      final GuestModel guest1 = billStateNotifier.state.guests[0];
+      final GuestModel guest2 = billStateNotifier.state.guests[1];
+
+      expect(dish.guestUuids, <String>[guest1.uuid, guest2.uuid]);
+      expect(guest1.dishes, <DishModel>[dish]);
+      expect(guest2.dishes, <DishModel>[dish]);
+    });
+
+    test('should edit tax successfully', () async {
+      billStateNotifier.editTax(22.0);
+      expect(billStateNotifier.state.tax, 22.0);
+    });
+
+    test('should edit split tax equally successfully', () async {
+      billStateNotifier.editSplitTaxEqually(isSplitTaxEqually: true);
+      expect(billStateNotifier.state.isSplitTaxEqually, true);
+    });
+
+  });
+}
