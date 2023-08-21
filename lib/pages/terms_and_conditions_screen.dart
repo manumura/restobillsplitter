67b5f2561a2ce677d4e78c29cfae18ad2c567403
Emmanuel Mura <emmanuel.mura@gmail.com>
2023-08-21 13:20:50 +0700

    update T&C + privacy

--- lib/pages/terms_and_conditions_screen.dart
+++ lib/pages/terms_and_conditions_screen.dart
@@ -30,7 +30,7 @@ By downloading or using the app, these terms will automatically apply to you –
     
 Emmanuel Mura is committed to ensuring that the app is as useful and efficient as possible. For that reason, we reserve the right to make changes to the app or to charge for its services, at any time and for any reason. We will never charge you for the app or its services without making it very clear to you exactly what you’re paying for.
 
-The Resto Bill Splitter app stores and processes personal data that you have provided to us, in order to provide my Service. It’s your responsibility to keep your phone and access to the app secure. We therefore recommend that you do not jailbreak or root your phone, which is the process of removing software restrictions and limitations imposed by the official operating system of your device. It could make your phone vulnerable to malware/viruses/malicious programs, compromise your phone’s security features and it could mean that the Resto Bill Splitter app won’t work  properly or at all.
+The Resto Bill Splitter app stores and processes personal data that you have provided to us, in order to provide our Service. It’s your responsibility to keep your phone and access to the app secure. We therefore recommend that you do not jailbreak or root your phone, which is the process of removing software restrictions and limitations imposed by the official operating system of your device. It could make your phone vulnerable to malware/viruses/malicious programs, compromise your phone’s security features and it could mean that the Resto Bill Splitter app won’t work  properly or at all.
 
 You should be aware that there are certain things that Emmanuel Mura will not take responsibility for. Certain functions of the app will require the app to have an active internet connection. The connection can be Wi-Fi, or provided by your mobile network provider, but Emmanuel Mura cannot take responsibility for the app not working at full functionality if you don’t have access to Wi-Fi, and you don’t have any of your data allowance left.
 
@@ -43,10 +43,10 @@ With respect to Emmanuel Mura’s responsibility for your use of the app, when y
 At some point, we may wish to update the app. The app is currently available on Android – the requirements for system (and for any additional systems we decide to extend the availability of the app to) may change, and you’ll need to download the updates if you want to keep using the app. Emmanuel Mura does not promise that it will always update the app so that it is relevant to you and/or works with the Android version that you have installed on your device. However, you promise to always accept updates to the application when offered to you, We may also wish to stop providing the app, and may terminate use of it at any time without giving notice of termination to you. Unless we tell you otherwise, upon any termination, (a) the rights and licenses granted to you in these terms will end; (b) you must stop using the app, and (if needed) delete it from your device.
 
 ## Changes to This Terms and Conditions
-I may update our Terms and Conditions from time to time. Thus, you are advised to review this page periodically for any changes. I will notify you of any changes by posting the new Terms and Conditions on this page. These changes are effective immediately after they are posted on this page.
+We may update our Terms and Conditions from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Terms and Conditions on this page. These changes are effective immediately after they are posted on this page.
 
 ## Contact Us
-If you have any questions or suggestions about my Terms and Conditions, do not hesitate to contact me at manumuradev@gmail.com.
+If you have any questions or suggestions about our Terms and Conditions, do not hesitate to contact us at manumuradev@gmail.com.
 
 Last December 29, 2020
 """;
